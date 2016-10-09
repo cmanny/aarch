cb347650e5234673d4f723f88cb1e7a7a1bbbe86
Callum Mann <cm13558@my.bristol.ac.uk>
2016-10-09 15:00:06 +0100

    add registers and made instructionset type

--- architecture/mappings.go
+++ architecture/mappings.go
@@ -57,6 +57,17 @@ const (
 	INS_CMP
 )
 
+const (
+  REG_A = iota
+  REG_B
+  REG_C
+  REG_D
+  REG_W
+  REG_X
+  REG_Y
+  REG_Z
+)
+
 type InsInfo struct {
 	ins_type int
 	op1      int
@@ -64,37 +75,75 @@ type InsInfo struct {
 	op3      int
 }
 
-var ins_map = make(map[int]InsInfo)
+type InstructionSet struct {
+  ins_map map[int]InsInfo
+  ins_id map[string]int
+}
 
-func MappingInit() {
+
+func (is* InstructionSet) Init() {
+  is.ins_map = make(map[int]InsInfo)
+  is.ins_id = make(map[string]int)
+
+
+  is.insMapInit()
+  is.strMapsInit()
+}
+
+func (is* InstructionSet) strMapsInit() {
+  is.ins_id["add"] = INS_ADD
+  is.ins_id["sub"] = INS_SUB
+  is.ins_id["mul"] = INS_MUL
+
+  is.ins_id["addi"] = INS_ADDI
+  is.ins_id["subi"] = INS_SUBI
+  is.ins_id["muli"] = INS_MULI
+
+  is.ins_id["lea"] = INS_LEA
+
+  is.ins_id["mov"] = INS_MOV
+  is.ins_id["movi"] = INS_MOVI
+
+  is.ins_id["ldr"] = INS_LDR
+  is.ins_id["str"] = INS_STR
+
+  is.ins_id["jeq"] = INS_JEQ
+  is.ins_id["jne"] = INS_JNE
+  is.ins_id["jl"] = INS_JL
+  is.ins_id["jge"] = INS_JGE
+
+  is.ins_id["cmp"] = INS_CMP
+}
+
+func (is* InstructionSet) insMapInit() {
 
 	/* Arithmetic */
-	ins_map[INS_ADD] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
-	ins_map[INS_SUB] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
-	ins_map[INS_MUL] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
+	is.ins_map[INS_ADD] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
+	is.ins_map[INS_SUB] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
+	is.ins_map[INS_MUL] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
 
-	ins_map[INS_ADDI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
-	ins_map[INS_SUBI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
-	ins_map[INS_MULI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	is.ins_map[INS_ADDI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	is.ins_map[INS_SUBI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	is.ins_map[INS_MULI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
 
-	ins_map[INS_LEA] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_ADDRESS, OP_CONSTANT}
+	is.ins_map[INS_LEA] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_ADDRESS, OP_CONSTANT}
 
 	/* Data movement */
-	ins_map[INS_MOV] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
-	ins_map[INS_MOVI] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	is.ins_map[INS_MOV] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	is.ins_map[INS_MOVI] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
 
-	ins_map[INS_LDR] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
-	ins_map[INS_STR] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	is.ins_map[INS_LDR] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	is.ins_map[INS_STR] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
 
 	/* Control flow */
 
-	ins_map[INS_JEQ] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
-	ins_map[INS_JNE] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
-	ins_map[INS_JL] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
-	ins_map[INS_JGE] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	is.ins_map[INS_JEQ] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	is.ins_map[INS_JNE] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	is.ins_map[INS_JL] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	is.ins_map[INS_JGE] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
 
 	/* Logical */
 
-	ins_map[INS_CMP] = InsInfo{INS_TYPE_LOGIC, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	is.ins_map[INS_CMP] = InsInfo{INS_TYPE_LOGIC, OP_REGISTER, OP_REGISTER, OP_EMPTY}
 
 }
