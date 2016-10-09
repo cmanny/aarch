80e23acfaf4f5a000963ec42ec30f4680f92e09a
Callum Mann <cm13558@my.bristol.ac.uk>
2016-10-09 11:48:20 +0100

    added mappings enumerations

--- architecture/mappings.go
+++ architecture/mappings.go
@@ -3,73 +3,98 @@ package architecture
 /* In enumerations */
 const (
 
-  /* Arithemtic instruction */
-  INS_ARITH = iota
+	/* Arithemtic instruction */
+	INS_TYPE_ARITH = iota
 
-  /* Control flow instruction */
-  INS_CONTROL
+	/* Control flow instruction */
+	INS_TYPE_CONTROL
 
-  /* Data movement instruction */
-  INS_MOVE
+	/* Data movement instruction */
+	INS_TYPE_MOVE
 
-  /* Logical operation */
-  INS_LOGIC
+	/* Logical operation */
+	INS_TYPE_LOGIC
 )
 
 /* Operand enumerations */
 const (
 
-  /* No instruction in this slot */
-  OP_EMPTY = iota
+	/* No instruction in this slot */
+	OP_EMPTY = iota
 
-  /* Memory address constant */
-  OP_ADDRESS
+	/* Memory address constant */
+	OP_ADDRESS
 
-  /* Register identifier */
-  OP_REGISTER
+	/* Register identifier */
+	OP_REGISTER
 
-  /* Constant value */
-  OP_CONSTANT
+	/* Constant value */
+	OP_CONSTANT
+)
+
+const (
+	INS_ADD = iota
+	INS_SUB
+	INS_MUL
+
+	INS_ADDI
+	INS_SUBI
+	INS_MULI
+
+	INS_LEA
+
+	INS_MOV
+	INS_MOVI
+
+	INS_LDR
+	INS_STR
+
+	INS_JEQ
+	INS_JNE
+	INS_JL
+	INS_JGE
+
+	INS_CMP
 )
 
 type InsInfo struct {
-  ins_type int
-  op1 int
-  op2 int
-  op3 int
+	ins_type int
+	op1      int
+	op2      int
+	op3      int
 }
 
-var ins_map = make(map[string]InsInfo)
+var ins_map = make(map[int]InsInfo)
 
 func MappingInit() {
 
-  /* Arithmetic */
-  ins_map["add"] = InsInfo{INS_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
-  ins_map["sub"] = InsInfo{INS_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
-  ins_map["mul"] = InsInfo{INS_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
+	/* Arithmetic */
+	ins_map[INS_ADD] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
+	ins_map[INS_SUB] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
+	ins_map[INS_MUL] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_REGISTER, OP_REGISTER}
 
-  ins_map["addi"] = InsInfo{INS_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
-  ins_map["subi"] = InsInfo{INS_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
-  ins_map["muli"] = InsInfo{INS_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	ins_map[INS_ADDI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	ins_map[INS_SUBI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	ins_map[INS_MULI] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
 
-  ins_map["lea"] = InsInfo{INS_ARITH, OP_REGISTER, OP_ADDRESS, OP_CONSTANT}
+	ins_map[INS_LEA] = InsInfo{INS_TYPE_ARITH, OP_REGISTER, OP_ADDRESS, OP_CONSTANT}
 
-  /* Data movement */
-  ins_map["mov"] = InsInfo{INS_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
-  ins_map["movi"] = InsInfo{INS_MOVE, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
+	/* Data movement */
+	ins_map[INS_MOV] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	ins_map[INS_MOVI] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_CONSTANT, OP_EMPTY}
 
-  ins_map["load"] = InsInfo{INS_MOVE, OP_REGISTER, OP_ADDRESS, OP_EMPTY}
-  ins_map["stor"] = InsInfo{INS_MOVE, OP_ADDRESS, OP_REGISTER, OP_EMPTY}
+	ins_map[INS_LDR] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	ins_map[INS_STR] = InsInfo{INS_TYPE_MOVE, OP_REGISTER, OP_REGISTER, OP_EMPTY}
 
-  /* Control flow */
+	/* Control flow */
 
-  ins_map["jeq"] = InsInfo{INS_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
-  ins_map["jne"] = InsInfo{INS_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
-  ins_map["jl"] = InsInfo{INS_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
-  ins_map["jge"] = InsInfo{INS_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	ins_map[INS_JEQ] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	ins_map[INS_JNE] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	ins_map[INS_JL] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
+	ins_map[INS_JGE] = InsInfo{INS_TYPE_CONTROL, OP_ADDRESS, OP_EMPTY, OP_EMPTY}
 
-  /* Logical */
+	/* Logical */
 
-  ins_map["cmp"] = InsInfo{INS_LOGIC, OP_REGISTER, OP_REGISTER, OP_EMPTY}
+	ins_map[INS_CMP] = InsInfo{INS_TYPE_LOGIC, OP_REGISTER, OP_REGISTER, OP_EMPTY}
 
 }
