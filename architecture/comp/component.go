68163e6d6a213a953797c7fe25f8da2db7dbd9d5
cmanny <cm13558@my.bristol.ac.uk>
2016-10-17 09:14:15 +0100

    communicator type

--- architecture/comp/component.go
+++ architecture/comp/component.go
@@ -5,4 +5,11 @@ type Component interface {
   Data() interface{}
   State() string
   Cycle() /* advance one cycle */
+  Communicator() /* retrieve the communicator */
+}
+
+/* All components need communicators */
+type Communicator struct {
+  Inputs map[string]chan interface{}
+  Outputs map[string]chan interface{}
 }
