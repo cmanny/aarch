a17841b089a5db3ddab2b1e517b97fc949557c91
cmanny <cm13558@my.bristol.ac.uk>
2016-10-19 22:29:26 +0100

    make exe of component type

--- architecture/comp/exe/arithmetic.go
+++ architecture/comp/exe/arithmetic.go
@@ -1,5 +1,14 @@
 package exe
 
+import (
+  "github.com/cmanny/aarch/architecture/ins"
+  "github.com/cmanny/aarch/architecture/comp"
+)
+
 type ArithmeticUnit struct {
 
+  Data() interface{}
+  State() string
+  Cycle()
+  Communicator() *Communicator
 }
--- architecture/comp/exe/control_flow.go
+++ architecture/comp/exe/control_flow.go
@@ -1,5 +1,14 @@
 package exe
 
+import (
+  "github.com/cmanny/aarch/architecture/ins"
+  "github.com/cmanny/aarch/architecture/comp"
+)
+
 type ControlUnit struct {
 
+  Data() interface{}
+  State() string
+  Cycle()
+  Communicator() *Communicator
 }
