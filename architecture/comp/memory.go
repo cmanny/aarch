0e8d530317127e86b9d133b8295bccca7d533b86
cmanny <cm13558@my.bristol.ac.uk>
2016-11-02 21:53:51 +0000

    added memory op enums

--- architecture/comp/memory.go
+++ architecture/comp/memory.go
@@ -1,12 +1,32 @@
 package comp
 
+const (
+  MEM_READ = iota
+  MEM_WRITE
+  MEM_RESET
+  MEM_CHAN1_IN
+  MEM_CHAN2_IN
+  MEM_CHAN3_IN
+  MEM_CHAN1_OUT
+  MEM_CHAN2_OUT
+  MEM_CHAN3_OUT
+)
+
+type MemOp struct {
+  op int
+  addr int
+  data []byte
+}
+
 type Memory struct {
   Communicator
   bytes [4096]byte
+  opsPerCycle int
 }
 
 func (mu *Memory) Init() {
   mu.InitComms()
+  mu.opsPerCycle = 2
   mu.Inputs["in1"] = make(chan interface{}, 1)
   mu.Outputs["out1"] = make(chan interface{}, 1)
 }
